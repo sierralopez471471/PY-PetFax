aa4e9b68595e9b0dbfe6567c18abb0476be0bc83
Sierra Lopez <sierralopez1006@gmail.com>
2023-03-03 13:47:13 -0800

    created app factory

--- app.py
+++ app.py
@@ -1,12 +1,3 @@
 # config
-from flask import Flask
-app = Flask(__name__)
-
-# index route
-@app.route('/')
-def index():
-    return 'Hello, this is PetFax!'
-
-@app.route('/pets')
-def pets():
-    return 'These are our pets available for adoption!'
+from petfax import create_app
+app = create_app()
