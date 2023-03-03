09210ea6f54f92d9d09cde64faaa0f48ed46b2b8
Sierra Lopez <sierralopez1006@gmail.com>
2023-03-03 14:40:50 -0800

    created pet blueprint and view

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -7,4 +7,8 @@ def create_app():
     def hello():
         return 'Hello, PetFax!'
     
+    # register pet blueprint
+    from . import pet
+    app.register_blueprint(pet.bp)
+    
     return app
